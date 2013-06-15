2aaf94fe1de5bab107c1c00ee83d4b57b886c796
duncan <duncan@suttree.com>
2013-06-15 11:10:28 +0200

    Parsing the social graph for new connections

--- laptye.rb
+++ laptye.rb
@@ -3,20 +3,43 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open('http://uk.linkedin.com/in/peterhgough'))
+i = 0
+url = 'http://uk.linkedin.com/in/peterhgough'
 
-puts "Current"
-puts "======="
-doc.css('.current li').each do |current|
-  current.css('.at').remove
-  current.css('.company-profile-public').remove
-  puts current.text.strip
-end
+begin
+  i += 1
 
-puts "Past"
-puts "===="
-doc.css('.past li').each do |past|
-  past.css('.at').remove
-  past.css('.company-profile-public').remove
-  puts past.text.strip
-end
+  doc = Nokogiri::HTML(open(url))
+
+  puts "Current"
+  puts "======="
+  doc.css('.current li').each do |current|
+    current.css('.at').remove
+    current.css('.company-profile-public').remove
+    puts current.text.strip
+  end
+
+  puts "Past"
+  puts "===="
+  doc.css('.past li').each do |past|
+    past.css('.at').remove
+    past.css('.company-profile-public').remove
+    puts past.text.strip
+  end
+
+  puts "Finding someone else"
+  links = []
+  doc.css('.browsemap .content li a').each do |link|
+    links << link['href']
+  end
+  url = links.sample(1).first
+
+  puts "This one"
+  puts url.inspect
+end while i < 5
+
+# Todo
+#
+# store job titles
+# browse randomly by picking profiles from the "viewers also viewed" sidebar
+# run continually
