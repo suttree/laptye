ab991fb598c469fdd71779aba18eb6f539777f97
duncan <duncan@suttree.com>
2013-06-15 23:14:45 +0200

    Better parsing, more profiles...

--- laptye.rb
+++ laptye.rb
@@ -1,22 +1,37 @@
 #! /usr/bin/ruby
 
+require 'mysql2'
 require 'nokogiri'
 require 'open-uri'
 
+#mysql> create table jobs (
+#       id INT primary key NOT NULL AUTO_INCREMENT,
+#       title text,
+#       next_job_id int,
+#       previous_job_id int
+#       );
+
 i = 0
 url = 'http://uk.linkedin.com/in/peterhgough'
+client = Mysql2::Client.new(:host => 'localhost', :username => 'root', :database => 'laptye')
 
 begin
   i += 1
 
   doc = Nokogiri::HTML(open(url))
+  next_job_id = nil
 
   puts "Current"
   puts "======="
   doc.css('.current li').each do |current|
     current.css('.at').remove
     current.css('.company-profile-public').remove
-    puts current.text.strip
+    title = current.text.strip.split(/\n/).first
+    puts title
+
+    client.query("INSERT INTO jobs (title) VALUES ('#{title}')")
+    result = client.query("SELECT LAST_INSERT_ID() AS next_id;")
+    next_job_id = result.first['next_id']
   end
 
   puts "Past"
@@ -24,7 +39,13 @@ begin
   doc.css('.past li').each do |past|
     past.css('.at').remove
     past.css('.company-profile-public').remove
-    puts past.text.strip
+    title = past.text.strip.split(/\n/).first
+    #puts title
+
+    client.query("INSERT INTO jobs (title, next_job_id) VALUES ('#{title}', #{next_job_id})")
+
+    result = client.query("SELECT LAST_INSERT_ID() AS next_id;")
+    next_job_id = result.first['next_id']
   end
 
   puts "Finding someone else"
@@ -36,10 +57,4 @@ begin
 
   puts "This one"
   puts url.inspect
-end while i < 5
-
-# Todo
-#
-# store job titles
-# browse randomly by picking profiles from the "viewers also viewed" sidebar
-# run continually
+end while i < 500
